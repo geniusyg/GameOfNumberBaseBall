52ba9755064e6d42af33d769da8a5c3f838b2f0d
Chae Yungi <l8673901@gmail.com>
2014-01-22 11:27:10 +0900

    UI 작업중..!!
    UI 작업중..!!

--- GameOfNumberBaseBall/AppDelegate.m
+++ GameOfNumberBaseBall/AppDelegate.m
@@ -7,12 +7,16 @@
 //
 
 #import "AppDelegate.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+	
+	
+	
     return YES;
 }
 							
